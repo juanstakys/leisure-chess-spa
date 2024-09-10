eaa7288ddde9ccf9afcf1a7bf55df0e240cf74a6
Juan Cruz Stakys <52844841+juanstakys@users.noreply.github.com>
2024-09-10 00:00:19 -0300

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import LoginButton from "./components/LoginButton.vue";
+import LogoutButton from "./components/LogoutButton.vue";
+import { TheChessboard } from "vue3-chessboard";
+import "vue3-chessboard/style.css";
+import { useAuth0 } from "@auth0/auth0-vue";
+
+const { user, isAuthenticated } = useAuth0();
+</script>
+
+<template>
+    <div v-if="isAuthenticated">
+      <img :src="user.picture" alt="User picture">
+      <p>{{ user.name }}</p>
+      <logout-button />
+    </div>
+    <login-button v-else />
+    <the-chessboard />
+</template>
+
+<style scoped></style>

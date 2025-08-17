80a39783aabc76e9d5e42c2b306c41bfa45b5bf1
Juan Cruz Stakys <52844841+juanstakys@users.noreply.github.com>
2025-08-17 15:50:37 -0300

    Replaced routing for simple conditional components. WIP managing turns and color asignment

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,48 @@
 <template>
     <h1>Chess Now!</h1>
-    <component
-        :is="currentView"
+    <Home @create="onCreate" @join="onJoin" v-if="!gameId" />
+    <Play
         :gameId="gameId"
-        @create="onCreate"
-        @join="onJoin"
+        :player-color="playerColor"
+        :ws="ws"
+        v-else-if="playerColor"
     />
 </template>
+S
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import Home from "./Home.vue";
 import Play from "./Play.vue";
-import NotFound from "./NotFound.vue";
 
-const routes = {
-    "/": Home,
-    "/play": Play,
-};
-
-const currentPath = ref(window.location.hash);
-
-let gameId;
-
-window.addEventListener("hashchange", () => {
-    currentPath.value = window.location.hash;
-});
-
-const currentView = computed(() => {
-    return routes[currentPath.value.slice(1) || "/"] || NotFound;
-});
+const gameId = ref("");
+const playerColor = ref("");
+let ws;
 
 async function onCreate() {
     const response = await fetch("http://localhost:3000");
-    gameId = await response.text();
-    window.location.hash = "/play";
+    gameId.value = await response.text();
+    console.log("gameId.value:", gameId.value);
+    ws = new WebSocket("ws://localhost:3001");
+    ws.onopen = () => {
+        ws.send(`${gameId.value} join`);
+    };
+    ws.onmessage = (event) => {
+        console.log("message:", event.data);
+        playerColor.value = event.data;
+    };
 }
 
 async function onJoin(gId) {
-    gameId = gId;
-    window.location.hash = "/play";
+    console.log("gameId.value:", gameId.value);
+    ws = new WebSocket("ws://localhost:3001");
+    ws.onopen = () => {
+        ws.send(`${gameId.value} join`);
+    };
+    ws.onmessage = (event) => {
+        console.log("message:", event.data);
+        playerColor.value = event.data;
+    };
+    gameId.value = gId;
 }
 </script>
-
-<style scoped></style>
--- src/Play.vue
+++ src/Play.vue
@@ -4,64 +4,47 @@
             <!-- Layout container: left = board, right = sidebar -->
             <section class="board">
                 <p>game ID: {{ gameId }}</p>
+                <p>Playing as {{ playerColor }}</p>
                 <!-- Chessboard area -->
                 <the-chessboard
                     @board-created="(api) => (board = api)"
                     @move="handleMove"
+                    :player-color="playerColor"
+                    :board-config="boardConfig"
                 />
-            </section>
-
-            <section class="sidebar" aria-label="Game sidebar">
-                <!-- Opponent/player labels -->
-                <div>
-                    <span>they</span>
-                </div>
-                <div>
-                    <span>you</span>
-                </div>
-
-                <!-- Move list -->
-                <section aria-labelledby="moves-title">
-                    <h2 id="moves-title">Moves</h2>
-                    <ol>
-                        <!-- moves will be listed here -->
-                    </ol>
-                </section>
-
-                <!-- Actions -->
-                <div>
-                    <button type="button">offer draw</button>
-                    <button type="button">resign</button>
-                </div>
             </section>
         </main>
     </div>
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref, computed } from "vue";
 import { TheChessboard } from "vue3-chessboard";
 import "vue3-chessboard/style.css";
 
-let ws;
 let board;
 
 const props = defineProps({
     gameId: String,
+    playerColor: String,
+    ws: WebSocket,
 });
 
+const boardConfig = {
+    orientation: props.playerColor,
+    position: "start",
+    draggable: {
+        enabled: true,
+    },
+};
+
 function handleMove({ san }) {
-    ws.send(`${props.gameId} move ${san}`);
+    props.ws.send(`${props.gameId} move ${san}`);
 }
 
 onMounted(() => {
-    ws = new WebSocket("ws://localhost:3001");
-    ws.onopen = () => {
-        ws.send(`${props.gameId} join`);
-    };
-    ws.onmessage = ({ data }) => {
-        console.log(data);
-        // TODO: allow resign and draw offering
+    props.ws.onmessage = ({ data }) => {
+        console.log("move:", data);
         board.move(data);
     };
 });
